ff140346911a6098288308a8306875997f9f2bc7
Vijay-eshwaran <vijay.eshwaran37@gmail.com>
2025-05-05 10:52:56 +0530

    Update Main.java made it more dynamic

--- lift/Main.java
+++ lift/Main.java
@@ -1,21 +1,39 @@
 package lift;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] aa) {
 
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("Enter user-name : ");
-		String name = sc.nextLine();
-		
-		User u1 = new User();
-		u1.setName(name);
-		u1.liftCreate();
-		u1.liftSystem();
-		System.out.println();
-		u1.alLiftPositions();
+        HashMap<String, User> users = new HashMap<>();
+
+        while (true) {
+            System.out.print("Enter user-name (or type 'exit' to quit): ");
+            String name = sc.nextLine().trim();
+
+            if (name.equalsIgnoreCase("exit")) {
+                System.out.println("Exiting lift system...");
+                break;
+            }
+
+            User user;
+            if (users.containsKey(name)) {
+                user = users.get(name);
+                System.out.println("Welcome back, " + name + "!");
+            } else {
+                user = new User();
+                user.setName(name);
+                
+                users.put(name, user);
+                System.out.println("New user created: " + name);
+            }
+            user.liftCreate();
+            user.liftSystem();
+            user.alLiftPositions();
+
+            System.out.println("-------------------------------\n");
+        }
 		
 	}
 
